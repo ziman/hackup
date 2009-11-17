f7d3eefd91df01ec85cc9ade50e2ad414deb62ab
Matus Tejiscak <ziman@functor.sk>
2009-11-17 23:00:52 +0100

    Deinit honors config.

--- Plugin/Deinit.hs
+++ Plugin/Deinit.hs
@@ -5,11 +5,13 @@ import Config
 import System.Directory
 
 run :: [String] -> IO ()
-run ["--really"] = do
-    removeDirectoryRecursive ".hackup"
+run args = deinit args =<< readConfig
+
+deinit ["--really"] config = do
+    removeDirectoryRecursive (fRoot config)
     putStrLn "Hackup deinitialized."
 
-run _ = putStr $ unlines
+deinit _ _ = putStr $ unlines
     [ "WARNING: This operation will irreversibly delete the .hackup directory."
     , "If you are sure you want this, use `hackup deinit --really'."
     ]    
