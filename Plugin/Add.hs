a28fe05d6c10035775bda0d3a1190898104ffc65
Matus Tejiscak <ziman@functor.sk>
2009-11-18 09:44:49 +0100

    hackup add: counting added

--- Plugin/Add.hs
+++ Plugin/Add.hs
@@ -8,14 +8,21 @@ import qualified FileHash
 import Data.List
 import System.Posix.Files
 import Control.Applicative
+import qualified Data.Map as M
 
 run :: Config -> [String] -> IO ()
 run _ [] = putStrLn "usage: hackup add <file> [<file> <file> ..∘]"
 
 run config args = do
     entries <- readEntries config
+    let oldEntries = M.fromList $ map (\e -> (name e, e)) entries
+        oldCount   = M.size oldEntries
     new <- mapM addFile args
-    writeEntries config $ new ++ filter ((`notElem` args) . name) entries
+    let newEntries = M.fromList $ map (\e -> (name e, e)) new
+    let finalEntries = newEntries `M.union` oldEntries
+        finalCount   = M.size newEntries
+    writeEntries config $ M.elems finalEntries
+    putStrLn $ show (finalCount - oldCount) ++ " file(s) added."
 
 addFile :: String -> IO Entry
 addFile fn = do
