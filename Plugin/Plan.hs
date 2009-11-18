508fbf7afeea3818a34565db96a02a45b5a4fadb
Matus Tejiscak <ziman@functor.sk>
2009-11-18 15:15:54 +0100

    Plan delete.

--- Plugin/Plan.hs
+++ Plugin/Plan.hs
@@ -8,10 +8,18 @@ import System.Directory
 
 run :: Config -> [String] -> IO ()
 run config ["create", pname] = createPlan config pname
+run config ["delete", pname] = deletePlan config pname
 run config _ = putStrLn "Usage: hackup plan (create <name>|delete <name>|list)"
 
 createPlan :: Config -> String -> IO ()
 createPlan config pname = do
     let planPath = fRoot config ++ "/plans/" ++ pname
     createDirectoryIfMissing True planPath
-    writeFile (planPath ++ "/dvd0001") "__FOO__"
+    writeFile (planPath ++ "/dvd0001") "__FOO__\n"
+    putStrLn "Plan created."
+
+deletePlan :: Config -> String -> IO ()
+deletePlan config pname = do
+    let planPath = fRoot config ++ "/plans/" ++ pname
+    removeDirectoryRecursive planPath
+    putStrLn "Plan deleted."
