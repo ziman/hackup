8197a0ab4b6c0014ba0dff79219a69c86853e608
Matus Tejiscak <ziman@functor.sk>
2009-11-18 15:13:35 +0100

    plan create stub.

--- Plugin/Plan.hs
+++ Plugin/Plan.hs
@@ -4,5 +4,14 @@ import Filelist
 import FileHash
 import Config
 
+import System.Directory
+
 run :: Config -> [String] -> IO ()
-run config args = putStrLn "Plan stub."
+run config ["create", pname] = createPlan config pname
+run config _ = putStrLn "Usage: hackup plan (create <name>|delete <name>|list)"
+
+createPlan :: Config -> String -> IO ()
+createPlan config pname = do
+    let planPath = fRoot config ++ "/plans/" ++ pname
+    createDirectoryIfMissing True planPath
+    writeFile (planPath ++ "/dvd0001") "__FOO__"
