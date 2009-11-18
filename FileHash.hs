7e39fd4a3222d03611b5b2c01b280a8035a80adf
Matus Tejiscak <ziman@functor.sk>
2009-11-18 17:14:09 +0100

    Prevent hashFile from leaving open files.

--- FileHash.hs
+++ FileHash.hs
@@ -4,6 +4,8 @@ import qualified Data.ByteString.Lazy as BS
 import Data.Word
 import Data.Function
 import Control.Parallel.Strategies
+import System.IO
+import Control.Applicative
 
 newtype Hash = Hash Word32 deriving (Read, Show, Eq, Ord)
 
@@ -14,4 +16,8 @@ hash :: BS.ByteString -> Hash
 hash = Hash . BS.foldl (\s c -> 3*s + fromIntegral c) 0xF0EC3D45
 
 hashFile :: String -> IO Hash
-hashFile fn = hash `fmap` BS.readFile fn
+hashFile fn = do
+    f <- openFile fn ReadMode
+    hash <- hash <$> BS.hGetContents f
+    hash `seq` hClose f
+    return hash
