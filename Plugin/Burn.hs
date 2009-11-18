34cb6cc3e723e8b444c78ea4979d7f2a31adbc85
Matus Tejiscak <ziman@functor.sk>
2009-11-18 18:08:25 +0100

    Some work on Burn.

--- Plugin/Burn.hs
+++ Plugin/Burn.hs
@@ -4,13 +4,30 @@ import Filelist
 import Config
 import Utils
 
+import Control.Applicative
+
 run :: Config -> [String] -> IO ()
-run config ("first":args) = resetBurner config >> burnNext config args
-run config ("next":args)  = burnNext config args
-run config _ = putStrLn "Usage: hackup burn (first|next)"
+run config ("first":pname:args) = resetBurner config pname "000000" >> burnNext config pname args
+run config ("next":pname:args)  = burnNext config pname args
+run config _ = putStrLn "Usage: hackup burn (first <plan-name>|next <plan-name>)"
 
-resetBurner :: Config -> IO ()
-resetBurner config = return ()
+resetBurner :: Config -> String -> String -> IO ()
+resetBurner config pname value = writeFile (fRoot config ++ "/plans/" ++ pname ++ "/burn") value
 
-burnNext :: Config -> [String] -> IO ()
-burnNext config args = return ()
+burnNext :: Config -> String -> [String] -> IO ()
+burnNext config pname args = do
+    current <- strictRead (planPath ++ "/burn")
+    resetBurner config pname (next current)
+    putStrLn $ "Burning " ++ current
+  where
+    planPath = fRoot config ++ "/plans/" ++ pname
+
+next :: String -> String
+next = fst . foldr next' ("", True)
+
+next' :: Char -> (String, Bool) -> (String, Bool)
+next' c (rest,flip)
+    | flip = case c of
+        '9' -> ('0':rest, True)
+        x   -> (succ x:rest, False)
+    | otherwise = (c:rest, False)
--- Utils.hs
+++ Utils.hs
@@ -1,14 +1,24 @@
 module Utils
     ( strictly
     , saneDirectoryContents
+    , strictRead
     )
     where
 
+import Control.Applicative
 import Control.Parallel.Strategies
 import System.Directory
+import System.IO
+
+strictRead :: FilePath -> IO String
+strictRead fname = do
+    f <- openFile fname ReadMode
+    stuff <- hGetContents f
+    rnf stuff `seq` hClose f
+    return stuff
 
 strictly :: NFData a => a -> a
-strictly x = x `using` rnf
+strictly x = rnf x `seq` x
 
 saneDirectoryContents :: FilePath -> IO [FilePath]
 saneDirectoryContents path = clean `fmap` getDirectoryContents path
