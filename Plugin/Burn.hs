5618711aa78b1cceea661fd8bfe3dd62c2b55e8e
Matus Tejiscak <ziman@functor.sk>
2009-11-18 17:46:16 +0100

    Burn: more developed stub.

--- Plugin/Burn.hs
+++ Plugin/Burn.hs
@@ -5,4 +5,12 @@ import Config
 import Utils
 
 run :: Config -> [String] -> IO ()
-run config args = putStrLn "Burn stub."
+run config ("first":args) = resetBurner config >> burnNext config args
+run config ("next":args)  = burnNext config args
+run config _ = putStrLn "Usage: hackup burn (first|next)"
+
+resetBurner :: Config -> IO ()
+resetBurner config = return ()
+
+burnNext :: Config -> [String] -> IO ()
+burnNext config args = return ()
